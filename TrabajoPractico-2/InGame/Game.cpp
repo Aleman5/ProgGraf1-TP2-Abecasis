b8837996fca747bc112c57cbfc9accb9f0e09042
Unknown <ale.bros@hotmail.com>
2018-04-24 16:53:54 -0300

    Third Push

--- TrabajoPractico-2/InGame/Game.cpp
+++ TrabajoPractico-2/InGame/Game.cpp
@@ -1,8 +1,31 @@
 #include"SFML\Audio.hpp"
 #include"SFML\System.hpp"
 #include"SFML\Window.hpp"
+#include"SFML\Graphics.hpp"
+
+//using namespace sf;
 
 void main() 
 {
-	
+	sf::RenderWindow window(sf::VideoMode(640, 480), "Game's Working");
+
+	while (window.isOpen())
+	{
+		sf::Event event;
+		
+		if (window.pollEvent(event)) {
+			switch (event.type)
+			{
+			case sf::Event::Closed:
+				window.close();
+				break;
+
+				default:
+					break;
+			}
+		}
+
+		window.clear();
+		window.display();
+	}
 }
